779725bcc673aef3ff080f36441ec2c5633d1c00
root <root@JellyFish.myguest.virtualbox.org>
2023-08-27 11:01:17 +0800

    编辑_ReadMe.md文档，设计用户交互过程；编辑dictionary.lisp脚本，使其具有存入单词和编辑单词信息的功能

--- dictionary.lisp
+++ dictionary.lisp
@@ -1,1 +1,13 @@
 "本脚本用于实现单词本"
+(defparameter *words-db* nil)
+
+(defun create-word (spell)
+  (copy-list `(:spell ,spell
+               :n nil :v nil
+               :adj nil :adv
+               :prep nil)))
+(defun add-word (word) (push word *words-db*))
+(defun set-word (word key value)
+  "设置特定单词的关键字值，value应为字符串"
+  (setf (getf word key) value))
+
