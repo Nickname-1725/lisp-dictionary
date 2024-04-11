e5416dfa6ea3bfaa8b6e89c1bcc239f94b7d7747
Nickname-1725 <1195019369@qq.com>
2024-04-11 23:49:07 +0800

    编写了宏的递归匹配展开测试示例，为trie-extract-word的编写提供指导

--- test_trie.lisp
+++ test_trie.lisp
@@ -67,3 +67,16 @@
     `(progn
        (format t "~a" (car ,x-list))
        (expand-list (quote ,(cdadr x-list))))))
+
+(defmacro expand-list-match (a-list x-list)
+  "一种宏递归匹配展开测试~@
+   能够自动处理a-list或x-list长度不足的问题"
+  (unless (or (eql nil x-list)
+              (eql nil (cadr x-list)))
+    `(progn
+       (if (eql (car ,a-list) (car ,x-list))
+           (progn
+             (cons (car ,x-list)
+              (expand-list-match
+               (quote ,(cdadr a-list))
+               (quote ,(cdadr x-list)))))))))
