184788e3f9c45290cdb0b8c282d7a072d126b419
Nickname-1725 <nickname-1725@outlook.com>
2024-05-21 23:33:05 +0800

    移除了注释, 调整了程序, 使其成为完整的应用, 编译并测试了新编的程序的功能, 略显粗糙, 对于页面跳转可增加清屏操作, 对于erase的wipe-clean可以修改reader, 使其什么都不做, 后续的y和n确认可以考虑使用图工具来构建

--- main.lisp
+++ main.lisp
@@ -1,4 +1,7 @@
 "本脚本用于实现单词本"
+(load "trie-store.lisp")
+(load "vocabulary.lisp")
+(load "flow-chart.lisp")
 (defparameter *words-db* nil)
 
 ;;;; 数据结构的存取、管理
@@ -8,23 +11,30 @@
                :adj nil :adv nil
                :prep nil)))
 (defun add-word (word) (push word *words-db*))
+; 其中(add-word(create-word spell))
+; 可以用(mark-word (add-word spell) (define-word spell))代替
 
 (defun set-word (word key value)
   "设置特定单词的关键字值，value应为字符串"
   (setf (getf word key) value))
+; 可以用(correct-def-by-id id class-string correct-string index)代替
+; 可以用(push-def-by-id id class-string def-string)代替
+; 可以用(remove-def-by-id id class-string index)代替
 (defun find-word (spell)
   "从字典中查找单词，若无则返回nil"
   (car (remove-if-not
         (lambda (word) (eql spell (getf word :spell)))
         *words-db*)))
+; 可以用(find-word spell)代替
 
-(set-word (find-word 'happy) :adj "快乐的")
 (defun remove-word-spell (spell)
   (setf *words-db* (remove-if
                     #'(lambda (word) (eq spell (getf word :spell)))
                     *words-db*)))
+; 暂无代替
 (defun clean-class-word (word key)
   (set-word word key nil))
+; 代替见上，或者也可以不代替
 
 (defun display-word (word)
   (flet ((display-class-word (word key)
@@ -37,6 +47,7 @@
     (display-class-word word :adv)
     (display-class-word word :prep)
     (format t "~%")))
+; 可以使用(describe-def-by-id id)
 
 ;;;; 数据库的存档与加载
 (defun save-db (data-base filename)
@@ -61,128 +72,79 @@
   (load-db *words-db* (concatenate 'string *config-root* "dictionary-words.db")))
 
 ;;;; 用户交互功能
-
-(defun user-read ()
-  "通用解析用户输入函数"
-  (let ((cmd (read-from-string
-              (concatenate 'string "(" (read-line) ")" ))))
-    (flet ((quote-it (x)
-             (list 'quote x)))
-      (cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
-
-(defmacro user-eval* (allow-cmds)
-  "模板，生成user-eval类型的函数，输入参数为允许的命令列表及允许词数
-  allow-cmds: 应形如((command-1 3) (command-2 1))"
-  `(lambda (sexp)
-     (format t "~c[2J~c[H" #\escape #\escape)
-     (let* ((allow-cmds ,allow-cmds)
-            (find-cmd (assoc (car sexp) allow-cmds)))
-       (if (and find-cmd
-                (eq (length sexp) (cadr find-cmd)))
-           (eval sexp)
-           (format t "Not a valid command. (✿ ◕ __ ◕ )~%")))))
-
 ;;; 子repl模板
 (defun user-cmd-description (cmd-desc)
   "依次打印命令的描述"
   (format t "~{~{- [~a~15t]: ~a~}~%~}" cmd-desc))
 
-(defparameter *the-word* nil)
-(defmacro user-repl* (cmd-desc u-eval)
+(defmacro user-repl* (cmd-desc)
   "子repl函数生成宏"
   `(lambda (spell)
-     (setf *the-word* (find-word spell))
-     (let ((word *the-word*))
+     (let ((word (find-word spell)))
        (labels
            ((repl (word)
               ; 此处显示查询单词的情况
-              (if *the-word*
+              (if word
                   (progn
-                    ;(format t "~c[2J~c[H" #\escape #\escape)
+                    (format t "~c[2J~c[H" #\escape #\escape)
                     (format t "The target *~a* found. (˵u_u˵)~%~%" spell)
                     (display-word word))
                   (progn
-                    ;(format t "~c[2J~c[H" #\escape #\escape)
+                    (format t "~c[2J~c[H" #\escape #\escape)
                     (format t "The taget *~a* does not exist. (ﾉ ◕ ヮ ◕ )ﾉ~%~%" spell)))
               ; 反馈可用命令
-              (user-cmd-description ,cmd-desc)
-              ; 执行用户命令
-              (let ((cmd (user-read)))
-                (if (eq (car cmd) 'back)
-                    (format t "~c[2J~c[H" #\escape #\escape)
-                    (progn (funcall ,u-eval cmd)
-                           (repl word))))))
-         (repl word)))))
+              (user-cmd-description ,cmd-desc)))
+         (repl word)
+         word))))
 
 ;; 主REPL命令集
-(defun note-down (spell)
-  ;(format t "~c[2J~c[H" #\escape #\escape)
-  (let ((word (find-word spell)))
-                                        ; 此处显示查询单词的情况
-    (if word
-        (progn
-          (format t "*~a* has already in our database.~%" spell)
-          (read-line))
-        (progn
-          (add-word (create-word spell))
-          (format t "The target *~a* has been add to our database.~%" spell)
-          (read-line)
-          (edit spell)))))
-
-(defparameter look-up-call
-  (user-repl*
-   '(("back" "go back to the main menu."))
-   (user-eval* '((back 1)))))
-(defparameter edit-call
-  (user-repl*
-   '(("back" "go back to the main menu.")
-     ("change :key new-meaning" "to change part of the speech of the target."))
-   (user-eval* '((back 1) (change 3)))))
-(defparameter erase-call
-  (user-repl*
-   '(("back" "go back to the main menu.")
-     ("wipe :key" "to wipe off part of the speech of the target.")
-     ("wipe-clean" "to wipe off the whole target clean."))
-   (user-eval* '((back 1) (wipe 2) (wipe-clean 1)))))
-(defmacro look-up (spell) `(funcall look-up-call ,spell))
-(defmacro edit (spell) `(funcall edit-call ,spell))
-(defmacro erase (spell) `(funcall erase-call ,spell))
-(defun restore ()
-  (save-words)
-  (format t "Neatly done.~%")
-  (read-line))
-(defun quit-the-main-repl ()
-  (save-words) ; 自动存档
-  (format t "The dictionary closed. Goodbye. (⌐ ■ ᴗ ■ )~%"))
+(defun look-up-func (spell)
+  (funcall
+   (user-repl*
+    '(("back" "go back to the main menu."))
+    ) spell))
+(defun edit-func (spell)
+  (funcall
+   (user-repl*
+    '(("back" "go back to the main menu.")
+      ("change :key new-meaning" "to change part of the speech of the target."))
+    ) spell))
+(defun erase-func (spell)
+  (funcall
+   (user-repl*
+    '(("back" "go back to the main menu.")
+      ("wipe :key" "to wipe off part of the speech of the target.")
+      ("wipe-clean" "to wipe off the whole target clean."))
+    ) spell))
 
 ;; 子repl命令集
-(defun change (key value)
-  (set-word *the-word* key (prin1-to-string value)))
-(defun wipe (key)
-  (clean-class-word *the-word* key))
-(defun wipe-clean ()
-  (if (not *the-word*)
+(defun change-func (word key value)
+  (set-word word key (prin1-to-string value)))
+(defun wipe-func (word key)
+  (clean-class-word word key))
+(defun wipe-clean-func (word)
+  (if (not word)
       (progn
         (format t "Quite clean. Nothing to wipe off.")
         (read-line))
       (progn (format t "are you sure you want to wipe the hole target *~a* clean? (˵u_u˵)[y/n]"
-                     (getf *the-word* :spell))
+                     (getf word :spell))
              (let* ((r-l (read-line))
                     (option (read-from-string
                              (if (eq (length r-l) 0)
                                  "default" r-l))))
                (cond ((eq 'y option)
-                      (remove-word-spell (getf *the-word* :spell))
-                      (setf *the-word* nil)
+                      (remove-word-spell (getf word :spell))
+                      (setf word nil)
                       (format t "~c[2J~c[H" #\escape #\escape)
                       (format t "Neatly-done.~%")
                       (read-line))
                      ((eq 'n option))
                      (t (format t "yes or no?[y/n]~%")
-                        (wipe-clean)))))))
+                        (wipe-clean-func word)))))))
 
-;; 主REPL
-(defun main-repl ()
+;;; 测试用例
+(flow-chart:def-init *repl-user* 'main
   (format t "The dictionary opened. Wellcome back. ( ✿ ◕ ‿ ◕ )~%")
   (user-cmd-description              ; 反馈可用命令
    '(("note-down spell" "note-down a word.")
@@ -190,23 +152,108 @@
      ("edit spell" "correct the fault.")
      ("erase spell" "give it a quick trim or eliminate it completely.")
      ("restore" "restore the data manually.")
-     ("quit" "close the dictionary. data will be automatically restored by your little helper.(˵ ✿ ◕ ‿ ◕ ˵)")))
-              ; 执行用户命令
-  (let ((cmd (user-read)))
-    (if (eq (car cmd) 'quit)
-        (quit-the-main-repl)
-        (progn
-          (funcall (user-eval*
-                    '((note-down 2)
-                      (look-up 2)
-                      (edit 2)
-                      (erase 2)
-                      (restore 1)
-                      (quit 1))) cmd)
-          (main-repl)))))
+     ("quit" "close the dictionary. data will be automatically restored by your little helper.(˵ ✿ ◕ ‿ ◕ ˵)"))))
+(flow-chart:def-arc *repl-user* 'main 'main '() ; 错误通配
+  (format t "~c[2J~c[H" #\escape #\escape)
+  (format t "Not a valid command. (✿ ◕ __ ◕ )~%")
+  'target)
+(flow-chart:def-arc *repl-user* 'main 'main '(quit) ; 退出程序
+  (save-words) ; 自动存档
+  (format t "The dictionary closed. Goodbye. (⌐ ■ ᴗ ■ )~%"))
+
+;; note-down
+(flow-chart:def-state *repl-user* 'note-down
+  nil)
+(flow-chart:set-state-reader
+ *repl-user* 'note-down
+ (macroexpand `(let* ((spell args)
+                      (word (find-word spell)))
+                 (if word '("fail") '("succeed")))))
+(flow-chart:def-arc *repl-user* 'main 'note-down '(note-down symbol)
+  (let ((args (cadr cmd-list))) 'target))
+(flow-chart:def-state *repl-user* 'note-down-succeed
+  (let ((spell args))
+    (format t "The target *~a* has been add to our database.~%" spell)))
+(flow-chart:def-arc *repl-user* 'note-down 'note-down-succeed '(succeed)
+  (let ((spell args))
+    (add-word (create-word spell))
+    'target))
+(flow-chart:def-state *repl-user* 'note-down-fail
+  (let ((spell args))
+    (format t "*~a* has already in our database.~%" spell)))
+(flow-chart:def-arc *repl-user* 'note-down 'note-down-fail '(fail)
+  'target)
+(flow-chart:def-arc *repl-user* 'note-down-fail 'main 'nil
+  'target)
+
+;; look-up
+(flow-chart:def-state *repl-user* 'look-up
+  (look-up-func args))
+(flow-chart:def-arc *repl-user* 'main 'look-up '(look-up symbol)
+  (let ((args (cadr cmd-list)))
+    'target))
+(flow-chart:def-arc *repl-user* 'look-up 'main '(back)
+  'target)
+(flow-chart:def-arc *repl-user* 'look-up 'look-up 'nil
+  (format t "~c[2J~c[H" #\escape #\escape)
+  (format t "Not a valid command. (✿ ◕ __ ◕ )~%")
+  'target)
+
+;; edit
+(flow-chart:def-state *repl-user* 'edit
+  (edit-func args))
+(flow-chart:def-arc *repl-user* 'main 'edit '(edit symbol)
+  (let ((args (cadr cmd-list)))
+    'target))
+(flow-chart:def-arc *repl-user* 'edit 'main '(back)
+  'target)
+(flow-chart:def-arc *repl-user* 'edit 'edit '(change symbol string)
+  (let ((key (cadr cmd-list))
+        (value (caddr cmd-list))
+        (spell args)
+        (args args))
+    (change-func (find-word spell) key value)
+    'target))
+(flow-chart:def-arc *repl-user* 'note-down-succeed 'edit 'nil
+  'target)
+(flow-chart:def-arc *repl-user* 'edit 'edit 'nil
+  (format t "~c[2J~c[H" #\escape #\escape)
+  (format t "Not a valid command. (✿ ◕ __ ◕ )~%")
+  'target)
+
+;; erase
+(flow-chart:def-state *repl-user* 'erase
+  (erase-func args))
+(flow-chart:def-arc *repl-user* 'main 'erase '(erase symbol)
+  (let ((args (cadr cmd-list)))
+    'target))
+(flow-chart:def-arc *repl-user* 'erase 'main '(back)
+  'target)
+(flow-chart:def-arc *repl-user* 'erase 'erase '(wipe symbol)
+  (let ((key (cadr cmd-list))
+        (spell args))
+    (wipe-func (find-word spell) key)
+    'target))
+(flow-chart:def-arc *repl-user* 'erase 'main '(wipe-clean)
+  (let ((spell args))
+    (wipe-clean-func (find-word spell))
+    'target))
+(flow-chart:def-arc *repl-user* 'erase 'erase 'nil
+  (format t "~c[2J~c[H" #\escape #\escape)
+  (format t "Not a valid command. (✿ ◕ __ ◕ )~%")
+  'target)
+
+;; store
+(flow-chart:def-state *repl-user* 'store
+  (format t "Neatly done.~%"))
+(flow-chart:def-arc *repl-user* 'main 'store '(store)
+  (save-words)
+  'target)
+(flow-chart:def-arc *repl-user* 'store 'main '()
+  'target)
 
 (defun init-fun ()
   (load-words) ; 自动加载存档
-  (main-repl)
-  (sleep 0.1)(quit))
-
+  (funcall (eval (flow-chart:diagram-realize *repl-user*)))
+  ;(sleep 0.1)
+  (quit))
